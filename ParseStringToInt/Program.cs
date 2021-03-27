3f0e09a067629fcda5e3fd00e0d1a9e6f2d3fad9
thalif <thalif12@gmail.com>
2021-03-27 14:13:45 +0530

    Add project files.

---
+++ ParseStringToInt/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using atoi;
+
+namespace ParseStringToInt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AtoiFunc Parser = new AtoiFunc();
+
+            int Value = Parser.Parse("2147.48364");
+            Console.WriteLine(Value.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ atoi/AtoiFunc.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atoi
+{
+    public class AtoiFunc
+    {
+        public int Parse(string _number)
+        {
+            int AsInteger = 0;
+            bool symbol = GetSymbol(_number);
+            for (int i = 0; i < _number.Length; i++)
+            {
+                if (AsInteger != 0 && !IsNumber(_number[i]) && _number[i] != '.')
+                    throw new FormatException();
+                else if (IsNumber(_number[i]))
+                {
+                    if ((AsInteger *= 10) < 0)
+                        throw new ArithmeticException();
+                    else
+                        AsInteger = AsInteger + GetIntForChar(_number[i]);
+                }
+                else if (AsInteger != 0)
+                    return PositiveNegative(symbol, AsInteger);
+            }
+            return PositiveNegative(symbol, AsInteger);
+        }
+        public bool IsSymbol(char item)
+        {
+            if (item == '-')
+                return true;
+            else if (item == '+')
+                return true;
+            return false;
+        }
+        public bool IsNumber(char item)
+        {
+            int n = item;
+            return n >= 48 && n <= 57;
+        }
+        public bool GetSymbol(string givenString)
+        {
+            foreach(char ccc in givenString)
+            {
+                if (ccc == '-')
+                    return false;
+                else if ((int)ccc >= 48 && (int)ccc <= 57)
+                    return true;
+            }
+            return true;
+        }
+        public int PositiveNegative(bool _item, int result)
+        {
+            if(!_item)
+                return result * -1;
+            return result;
+        }
+        public int GetIntForChar(char _char)
+        {
+            return _char - 48;
+        }
+        public AtoiFunc()
+        {
+            
+        }
+    }
+}
